acc17e1f5c1383d7bfa2bb81777282cf610cd976
Software Scientist <sheerazali656@gmail.com>
2022-09-20 12:21:54 +0500

    I am rich app
    le Bol police wale I love your chocolaty sister so so much your sister Kinza Amjad is so so much chocolaty haaaaaaaa

---
+++ I Am rich/I Am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am rich
+//
+//  Created by Sheeraz Ali on 17/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
